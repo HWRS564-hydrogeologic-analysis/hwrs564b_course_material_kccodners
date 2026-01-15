5678d9260e978440271c561f3b78d5d65a959aac
Laura Condon <lecondon@email.arizona.edu>
2026-01-15 16:05:45 -0700

    Setting up environment and code space

---
+++ postbuild.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+echo "------------------------------------------------"
+echo `pwd`
+echo "------------------------------------------------"
+
+cd_prefix="./hwrs564b_course_*"
+dirs=( $cd_prefix )
+if [ ${#dirs[@]} -eq 1 ] && [ -d "${dirs[0]}" ]; then
+    cd "${dirs[0]}"
+else
+    echo "Error: expected exactly one directory matching $cd_prefix, found ${#dirs[@]}"
+fi
+
+
+echo "------------------------------------------------"
+echo `pwd`
+echo "------------------------------------------------"
+
+uv venv
+uv sync
+source ./.venv/bin/activate
+cd ..
+mkdir -p ./modflow
+python -c "from flopy.utils import get_modflow; get_modflow('./modflow')"
+python -m ipykernel install --user --name hwrs564a
